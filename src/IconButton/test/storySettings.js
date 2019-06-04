0dd375eaa0c22a1283cc36d87dff307f4f25fcc5
TalZa <talza@wix.com>
2019-06-04 17:01:20 +0300

    iconButton2

--- src/IconButton/test/storySettings.js
+++ src/IconButton/test/storySettings.js
@@ -4,10 +4,15 @@ export const storySettings = {
   kind: Category.BUTTONS,
   category: Category.BUTTONS,
   storyName: '5.2 IconButton',
+  dataHook: 'iconbutton-icon',
 };
 
 export const testStories = {
   ICONBUTTON_SKINS: 'IconButton skins',
   ICONBUTTON_SIZES: 'IconButton sizes',
   ICONBUTTON_AS: 'IconButton as',
+};
+
+export const dataHooks = {
+  iconOfIconButton: 'iconbutton-icon',
 };
