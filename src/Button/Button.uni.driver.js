d5f6ece8d72bad3d922c83f270b5b08634d53f53
TalZa <talza@wix.com>
2019-06-30 18:11:14 +0300

    migrating button unidriver from "wix-ui-core"

--- src/Button/Button.uni.driver.js
+++ src/Button/Button.uni.driver.js
@@ -1,3 +1,19 @@
-import { buttonNextDriverFactory } from 'wix-ui-core/drivers/unidriver';
+import { baseUniDriverFactory } from 'wix-ui-test-utils/base-driver';
+import { StylableUnidriverUtil } from 'wix-ui-test-utils/unidriver';
+import styles from 'wix-ui-core/dist/src/components/button-next/button-next.st.css';
+
+const buttonNextDriverFactory = base => {
+  const stylableUtil = new StylableUnidriverUtil(styles);
+
+  return {
+    ...baseUniDriverFactory(base),
+    getButtonTextContent: async () => base.text(),
+    isFocused: async () => document.activeElement === (await base.getNative()), // eslint-disable-line no-restricted-properties
+    isButtonDisabled: async () => {
+      // Using stylable state and not html 'disabled' attribute, since if 'href' exists, then we don't pu the 'disabled' attribute.
+      return stylableUtil.hasStyleState(base, 'disabled');
+    },
+  };
+};
 
 export const buttonDriverFactory = buttonNextDriverFactory;
